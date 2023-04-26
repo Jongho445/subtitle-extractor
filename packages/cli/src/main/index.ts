d393a6152eabe19699a75a2c657265a4f550a877
Jongho Kim <zc798546@gmail.com>
2023-04-27 03:48:56 +0900

    feat: add inquirer

--- packages/cli/src/main/index.ts
+++ packages/cli/src/main/index.ts
@@ -1,3 +1,10 @@
 import {hello} from "extractor";
+import inquirer from "inquirer";
 
-console.log(hello);
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'first_name',
+    message: hello + " What's your first name",
+  }
+]);
